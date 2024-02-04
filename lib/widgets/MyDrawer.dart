d0f9f4e604776732a999764d5c4ea9d718c8f19e
Amaanarabiyani <amaanmemon200@gmail.com>
2024-02-04 11:08:21 +0530

    updated

--- lib/widgets/MyDrawer.dart
+++ lib/widgets/MyDrawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -37,6 +38,171 @@ class MyDrawer extends StatelessWidget {
             currentAccountPicture: CircleAvatar(
               backgroundImage: AssetImage(
                 'assets/images/cir.png',
+              ),
+            ),
+          ),
+          ListTile(
+            trailing: Icon(
+              Icons.arrow_forward_ios_sharp,
+            ),
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
+              ),
+            ),
+          ),
+          ListTile(
+            leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/profile.png',
+              ),
+            ),
+            title: Text(
+              'Profile',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 13,
+                fontFamily: 'mon',
               ),
             ),
           ),
